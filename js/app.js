878d84d99270f2a265c5afd3009bc133759953e5
Harry Pujols <harrypujols@me.com>
2017-01-23 23:31:50 -0500

    vuejs 2

--- js/app.js
+++ js/app.js
@@ -1,3 +1,34 @@
+Vue.component('window', {
+  template: '#window'
+})
+
+Vue.directive('draggable', {
+  bind: function(el) {
+    var drag = this.drag
+
+    el.onmousedown = function(e) {
+      drag = true
+      this.classList.add('dragging')
+    },
+
+    el.onmousemove = function(e) {
+      var w = this.offsetWidth / 2
+      var h = this.offsetHeight / 2
+      if ( drag === true ) {
+        var x = e.clientX - w
+        var y = e.clientY - h
+        var pos = 'position:absolute; left:' + x + 'px; top:' + y + 'px;'
+        this.setAttribute('style', pos)
+      }
+    },
+
+    el.onmouseup = function() {
+      drag = false;
+      this.classList.remove('dragging')
+    }
+  }
+})
+
 new Vue({
   el: '#desktop',
   data: {
@@ -5,43 +36,11 @@ new Vue({
     drag: true
   },
 
-  directives: {
-    include: function() {
-      var url = this.expression
-      var that = this
-      var request = new XMLHttpRequest()
-      request.open('GET', url, true)
-      request.onreadystatechange = function() {
-        if ( this.readyState !== 4 || this.status !== 200 ) return
-        that.el.innerHTML = this.responseText
-      }
-      request.send();
-    },
-
-    draggable: {
-      bind: function() {
-        var drag = this.drag
-        this.el.onmousedown = function(e) {
-          drag = true
-          this.classList.add('dragging')
-        },
-        this.el.onmousemove = function(e) {
-          var w = this.offsetWidth / 2
-          var h = this.offsetHeight / 2
-          if ( drag === true ) {
-            var x = e.clientX - w
-            var y = e.clientY - h
-            var pos = 'position:absolute;left:' + x + 'px;top:' + y + 'px;'
-            this.setAttribute('style', pos)
-          }
-        },
-        this.el.onmouseup = function() {
-          drag = false;
-          this.classList.remove('dragging')
-        }
-      }
-    }
-  },
+  // directives: {
+  //   draggable: {
+  //
+  //   }
+  // },
 
   methods: {
 
