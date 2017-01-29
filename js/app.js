0162e94c877d61edf9eb1ab497807ebffea43037
Harry Pujols <harrypujols@me.com>
2017-01-28 19:07:23 -0500

    directive general :(

--- js/app.js
+++ js/app.js
@@ -1,3 +1,35 @@
+Vue.directive('draggable', {
+  bind: function(el) {
+    var drag = this.drag
+
+    el.onmousedown = function(e) {
+      drag = true
+      this.classList.add('dragging')
+    },
+
+    el.onmousemove = function(e) {
+      var w = this.offsetWidth / 2
+      var h = this.offsetHeight / 2
+      if ( drag === true ) {
+        var x = e.clientX - w
+        var y = e.clientY - h
+        var pos = 'position:absolute; left:' + x + 'px; top:' + y + 'px;'
+        this.setAttribute('style', pos)
+      }
+    },
+
+    el.onmouseup = function() {
+      drag = false;
+      this.classList.remove('dragging')
+    }
+  }
+});
+
+// Vue.component('window', {
+//   template: '#window',
+//   props: ['draggable']
+// });
+
 new Vue({
   el: '#desktop',
 
@@ -12,32 +44,32 @@ new Vue({
     }
   },
 
-  directives: {
-    draggable: {
-      bind: function() {
-        var drag = this.drag
-
-        el.onmousedown = function(e) {
-          drag = true
-          this.classList.add('dragging')
-        },
-
-        el.onmousemove = function(e) {
-          var w = this.offsetWidth / 2
-          var h = this.offsetHeight / 2
-          if ( drag === true ) {
-            var x = e.clientX - w
-            var y = e.clientY - h
-            var pos = 'position:absolute;left:' + x + 'px;top:' + y + 'px;'
-            this.setAttribute('style', pos)
-          }
-        },
-
-        el.onmouseup = function() {
-          drag = false;
-          this.classList.remove('dragging')
-        }
-      }
-    }
-  }
+  // directives: {
+  //   draggable: {
+  //     bind: function(el) {
+  //       var drag = this.drag
+  //
+  //       el.onmousedown = function(e) {
+  //         drag = true
+  //         this.classList.add('dragging')
+  //       },
+  //
+  //       el.onmousemove = function(e) {
+  //         var w = this.offsetWidth / 2
+  //         var h = this.offsetHeight / 2
+  //         if ( drag === true ) {
+  //           var x = e.clientX - w
+  //           var y = e.clientY - h
+  //           var pos = 'position:absolute; left:' + x + 'px; top:' + y + 'px;'
+  //           this.setAttribute('style', pos)
+  //         }
+  //       },
+  //
+  //       el.onmouseup = function() {
+  //         drag = false;
+  //         this.classList.remove('dragging')
+  //       }
+  //     }
+  //   }
+  // }
 });
