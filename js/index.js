a639de18991779976d7bb079caeb7bd506d0f0a0
Harry Pujols <harrypujols@me.com>
2017-05-24 16:10:00 -0400

    Passing data with axios

--- js/index.js
+++ js/index.js
@@ -1,23 +1,47 @@
 import draggable from './directives/draggable'
 import include from './directives/include'
 
+Vue.prototype.$http = axios
+
 new Vue({
   el: '#desktop',
 
   data: {
-    message: 'window',
+    datafile: '../data/data.json',
+    message: 'hello',
+    content: {},
     drag: true
   },
 
   components: {
     'window': {
       template: '#window',
-      props: ['text'],
+      props: ['post'],
       data: function() {
         return {
           items: [1, 2, 3, 4]
         }
       }
     }
+  },
+
+  methods: {
+    update: function() {
+      var that = this
+      this.$http({
+        url: this.datafile,
+        method: 'GET'
+      })
+      .then(function(result) {
+        that.content = result.data
+      })
+      .catch(function(error){
+        console.log(error)
+      })
+    }
+  },
+
+  mounted: function() {
+    this.update()
   }
 });
