4a10d59956ca0998e7d37caaaaf8f4404473bf2a
Harry Pujols <harrypujols@me.com>
2017-08-14 14:52:23 -0400

    Draggable directive with Interact.js

--- src/js/directives/draggable.js
+++ src/js/directives/draggable.js
@@ -1,27 +1,34 @@
 // draggable
 export default Vue.directive('draggable', {
   bind: function(el) {
-    var drag = el.drag
+    interact(el, {
+      allowFrom: el.querySelector('.drag-point')
+    })
+    .draggable({
 
-    el.onmousedown = function(e) {
-      drag = true
-      el.classList.add('dragging')
-    },
+      restrict: {
+        restriction: '#screen',
+        endOnly: true,
+        elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+      },
 
-    el.onmousemove = function(e) {
-      var w = el.offsetWidth / 2
-      var h = el.offsetHeight / 2
-      if ( drag === true ) {
-        var x = e.clientX - w
-        var y = e.clientY - h
-        var pos = 'position:absolute; left:' + x + 'px; top:' + y + 'px;'
-        el.setAttribute('style', pos)
-      }
-    },
+      onmove: dragMoveListener
+    })
 
-    el.onmouseleave = function() {
-      drag = false;
-      el.classList.remove('dragging')
+    function dragMoveListener (event) {
+      var target = event.target,
+      x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+      y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+      target.style.webkitTransform =
+      target.style.transform =
+        'translate(' + x + 'px, ' + y + 'px)';
+
+      target.setAttribute('data-x', x);
+      target.setAttribute('data-y', y);
     }
+
+    window.dragMoveListener = dragMoveListener;
   }
+
 })
