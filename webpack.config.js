766f7ecc23a5dd05a2750daf3f4ea1d083e745c6
Harry Pujols <harrypujols@me.com>
2017-11-18 12:37:03 -0500

    fixing font issue

--- webpack.config.js
+++ webpack.config.js
@@ -28,15 +28,11 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
+        use: 'file-loader'
       },
       {
          test: /\.(woff|woff2|eot|ttf|otf)$/,
-         use: [
-           'file-loader'
-         ]
+         use: 'file-loader'
       }
     ],
   }
