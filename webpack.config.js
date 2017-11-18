d903afacd6cfae08ec6308f97b30157eb6a401d2
Harry Pujols <harrypujols@me.com>
2017-11-18 12:40:04 -0500

    changing loaders

--- webpack.config.js
+++ webpack.config.js
@@ -28,11 +28,11 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: 'file-loader'
+        use: 'url-loader'
       },
       {
          test: /\.(woff|woff2|eot|ttf|otf)$/,
-         use: 'file-loader'
+         use: 'url-loader'
       }
     ],
   }
